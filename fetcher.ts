85260c73d38d454c828045f6fed4c0d973da911c
Kristián Kunc <kristian.kunc@gmail.com>
2025-10-08 14:09:04 +0200

    re-fetch surebets entierly

--- fetcher.ts
+++ fetcher.ts
@@ -125,22 +125,13 @@ export async function main() {
 
 	const fetchedSurebets = await loadSurebets(page);
 	const surbetsToAlert = [];
+	surebets.clear();
 
 	for (const surebet of fetchedSurebets) {
 		surebets.set(surebet.id, surebet);
 		if (surebet.profitPercent >= Number(process.env.ALERT_THRESHOLD)!) {
 			surbetsToAlert.push(surebet);
 		}
-	}
-
-	for (const id of surebets.keys()) {
-		const surebet = surebets.get(id);
-
-		// check if time is in the past + 6 hours
-		if (surebet!.time < new Date(Date.now() - 6 * 60 * 60 * 1000)) surebets.delete(id);
-
-		const res = await fetch(surebet!.generateCalculatorUrl());
-		if (res.status === 404) surebets.delete(id);
 	}
 
 	await alertSurebets(surbetsToAlert);
